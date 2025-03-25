465d64ead0ceb110cc4f06e203c56e5a3dead477
webgalacticdev <webgalacticdev@proton.me>
2025-03-25 15:23:25 +0200

    test

--- client/src/components/Navigate/Navigate.tsx
+++ client/src/components/Navigate/Navigate.tsx
@@ -4,10 +4,24 @@ import styles from './Navigate.module.css';
 const Navigate = () => {
   return (
     <div className={styles.container}>
-      <Link to='/' className={styles.logo}>
-        Questionnaire Builder
+      <Link to='/' className={styles.logoLink}>
+        <span className={styles.logo}>qb</span>
+        <span className={styles.logoText}>Questionnaire Builder</span>
       </Link>
       <button className={styles.button}>
+      <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.25rem"
+    height="1.25rem"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+  >
+    <path d="M12 19v-7m0 0V5m0 7H5m7 0h7"></path>
+    </svg>
         <Link className={styles.link} to='/create'>Create</Link>
       </button>
     </div>
