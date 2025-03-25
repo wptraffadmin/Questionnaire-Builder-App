04aaf9883bb2279333041a4cfa529f28bfe4bf57
webgalacticdev <webgalacticdev@proton.me>
2025-03-25 11:12:14 +0200

    test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'modern-normalize/modern-normalize.css';
+import 'modern-normalize'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
